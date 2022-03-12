fd610d0ebc9af9516c7b1bb68812ea3db7ec9312
Vinicius Vassão <vlviniciusguaruja7@gmail.com>
2022-03-11 23:00:16 -0300

    style(catalog): linting on migrations

--- catalog-api/Broooms.Catalog/Data/Migrations/20220312015857_CreateProductTable.cs
+++ catalog-api/Broooms.Catalog/Data/Migrations/20220312015857_CreateProductTable.cs
@@ -1,48 +1,39 @@
-﻿using System;
+﻿#nullable disable
+
+namespace Broooms.Catalog.Data.Migrations;
+
 using Microsoft.EntityFrameworkCore.Migrations;
 
-#nullable disable
-
-namespace Broooms.Catalog.Data.Migrations
+public partial class CreateProductTable : Migration
 {
-    public partial class CreateProductTable : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Products",
-                columns: table =>
-                    new
-                    {
-                        Id = table.Column<Guid>(type: "uuid", nullable: false),
-                        Name = table.Column<string>(
-                            type: "character varying(100)",
-                            maxLength: 100,
-                            nullable: false
-                        ),
-                        Description = table.Column<string>(
-                            type: "character varying(1000)",
-                            maxLength: 1000,
-                            nullable: false
-                        ),
-                        Price = table.Column<decimal>(type: "numeric", nullable: false),
-                        Quantity = table.Column<int>(type: "integer", nullable: false),
-                        ImageUrl = table.Column<string>(
-                            type: "character varying(300)",
-                            maxLength: 300,
-                            nullable: true
-                        )
-                    },
-                constraints: table =>
+    protected override void Up(MigrationBuilder migrationBuilder) =>
+        migrationBuilder.CreateTable(
+            name: "Products",
+            columns: table =>
+                new
                 {
-                    table.PrimaryKey("PK_Products", x => x.Id);
-                }
-            );
-        }
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(
+                        type: "character varying(100)",
+                        maxLength: 100,
+                        nullable: false
+                    ),
+                    Description = table.Column<string>(
+                        type: "character varying(1000)",
+                        maxLength: 1000,
+                        nullable: false
+                    ),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    ImageUrl = table.Column<string>(
+                        type: "character varying(300)",
+                        maxLength: 300,
+                        nullable: true
+                    )
+                },
+            constraints: table => table.PrimaryKey("PK_Products", x => x.Id)
+        );
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(name: "Products");
-        }
-    }
+    protected override void Down(MigrationBuilder migrationBuilder) =>
+        migrationBuilder.DropTable(name: "Products");
 }
